7f2769ff23c2d46db02c4cfabbe1fe743b5e24f4
bshongwe <dru.da.beat.hustle@gmail.com>
2024-04-14 08:35:47 +0200

    Data model defitions

---
+++ Models/AccountHolder.cs
@@ -0,0 +1,13 @@
+// Ernest B. Shongwe
+// Data Model Defs
+public class AccountHolder
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string IDNumber { get; set; }
+    public string ResidentialAddress { get; set; }
+    public string MobileNumber { get; set; }
+    public string EmailAddress { get; set; }
+}
